171957fe9ede24e52fda0373a4d3b1bb98064967
qixy <qdigest@gmail.com>
2017-03-03 10:27:59 +0800

    init project

---
+++ Pop3MailDownloader.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#@Filename : Pop3MailDownloader
+#@Date : 2017-03-03-09-37
+#@Poject: emaildownloader
+#@AUTHOR : footballqq
+
+import poplib
+import email
+import os
+import ConfigParser
+
+def get_config():
+    pass
+
+def get_emails(cfg):
+    pass
+
+def main():
+    cfg = get_config()
+    emails = get_emails(cfg)
+
+if __name__=='__main__':
+    main()
